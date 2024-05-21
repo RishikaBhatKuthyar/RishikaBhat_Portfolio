ddc27ed435182aaba99d391401cc4c3a832dc4ab
RishikaBhatKuthyar <kuthyar.ri@northeastern.edu>
2024-05-21 12:12:12 -0400

    removed unused imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
-  AiFillGithub,
-  AiFillInstagram,
+  AiFillGithub
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
