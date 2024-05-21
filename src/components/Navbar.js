26bcfa112901d0fec6a3aef1fc1d3bf04b58f558
RishikaBhatKuthyar <kuthyar.ri@northeastern.edu>
2024-05-21 11:57:24 -0400

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/rishikabhat/Portfolio"
+                href="https://github.com/RishikaBhatKuthyar/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
